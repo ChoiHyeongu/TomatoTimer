30440ff870f3ed14ef90e2ece1f9cd63ec50ca24
ChoiHyeongu <a11dsd123@gmail.com>
2018-04-08 23:44:46 +0900

    added controllButon click method and text size up

--- app/src/main/java/a/choi/tomatotimer/MainActivity.java
+++ app/src/main/java/a/choi/tomatotimer/MainActivity.java
@@ -2,6 +2,9 @@ package a.choi.tomatotimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        final Button controll = (Button) findViewById(R.id.controllButton);
+        controll.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                controll.setText("멈춤");
+            }
+        });
     }
 }
