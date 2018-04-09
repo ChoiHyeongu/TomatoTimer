e634a9f2f4e9460cb1e634cac853f977fe103ae0
ChoiHyeongu <a11dsd123@gmail.com>
2018-04-09 23:50:15 +0900

    added TimerOn function but incomplete

--- app/src/main/java/a/choi/tomatotimer/MainActivity.java
+++ app/src/main/java/a/choi/tomatotimer/MainActivity.java
@@ -1,10 +1,16 @@
 package a.choi.tomatotimer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +24,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 controll.setText("멈춤");
+                TimerOn();
             }
         });
+    }
+
+    public void TimerOn(){
+
+        final TextView timeView  = (TextView) findViewById(R.id.timeView);
+
+        Timer mTimer;
+        TimerTask mTask;
+        int count = 0;
+
+        mTask=new TimerTask() {
+            @Override
+            public void run() {
+                timeView.setText("25:00");
+            }
+        };
+        mTimer=new Timer();
+        mTimer.scheduleAtFixedRate(mTask,0,2000);
     }
 }
