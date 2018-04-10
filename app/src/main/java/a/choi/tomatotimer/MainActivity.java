d31998ed2cba478b5bffde60738595fedeed5442
ChoiHyeongu <a11dsd123@gmail.com>
2018-04-10 23:52:02 +0900

    added counter, removed round and today textview, change button shape

--- app/src/main/java/a/choi/tomatotimer/MainActivity.java
+++ app/src/main/java/a/choi/tomatotimer/MainActivity.java
@@ -1,5 +1,6 @@
 package a.choi.tomatotimer;
 
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +13,14 @@ import org.w3c.dom.Text;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+        private int count = 60;
+        private int MILLISINFUTURE = count *1000;
+        private static final int COUNT_DOWN_INTERVAL = 1000;
+        private boolean status = true;  // true -> start false -> stop
+        private CountDownTimer countDownTimer;
+        private int second, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,27 +31,64 @@ public class MainActivity extends AppCompatActivity {
         controll.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                controll.setText("멈춤");
-                TimerOn();
+
+                if(status == true) {
+                    countDownTimer();
+                    countDownTimer.start();
+                    controll.setText("■");
+                    status = !status;
+                }
+                else{
+                    countDownTimer.cancel();
+                    controll.setText("▶");
+                    status = !status;
+                }
             }
         });
     }
 
-    public void TimerOn(){
+    public void countDownTimer(){
 
-        final TextView timeView  = (TextView) findViewById(R.id.timeView);
+        final TextView timeView = (TextView) findViewById(R.id.timeView);
 
-        Timer mTimer;
-        TimerTask mTask;
-        int count = 0;
+        countDownTimer = new CountDownTimer(MILLISINFUTURE, COUNT_DOWN_INTERVAL) {
+            public void onTick(long millisUntilFinished) {
+                if(count == 0)
+                    countDownTimer.onFinish();
 
-        mTask=new TimerTask() {
-            @Override
-            public void run() {
-                timeView.setText("25:00");
+                timeView.setText(timeText(count));
+                count--;
+            }
+            public void onFinish() {
+                timeView.setText("00:00");
             }
         };
-        mTimer=new Timer();
-        mTimer.scheduleAtFixedRate(mTask,0,2000);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try{
+            countDownTimer.cancel();
+        } catch (Exception e) {}
+        countDownTimer=null;
+    }
+
+    public String timeText(int count){
+
+        minute = count / 60;
+        second = count % 60;
+
+        if(minute < 10 && second <10)
+            return "0"+minute+":"+"0"+second;
+
+        else if(minute < 10 )
+            return "0"+minute+":"+second;
+
+        else if(second < 10)
+            return minute+":"+"0"+second;
+
+        else 
+            return minute + ":" + second;
     }
 }
